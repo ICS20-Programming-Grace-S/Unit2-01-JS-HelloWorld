e9d09c203092eefab11379f78d1f9d1e060ab85b
Grace Silvaroli <99274228+GraceSilvaroli@users.noreply.github.com>
2022-03-10 04:15:58 +0000

    edit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-// Copyright (c) 2022 Ms Raffin All rights reserved
+// Copyright (c) 2022 Grace s All rights reserved
 //
-// Created by: Ms Raffin
+// Created by: Grace S
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
 
 function myButtonClicked () {
-  // this function displays "Hello, World!"
+  // this function displays "My Text for Button!"
   
   document.getElementById('hello-world').innerHTML = 'Woah! This is a new feature I just learned. Instead of those annoying pop up windows, behold...'
 }
